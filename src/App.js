5e816196502777bcc91bd6259b8bbdd5736c2dd7
ALEM Guillaume <Guillaume.ALEM@maif.fr>
2021-08-29 19:36:19 +0200

    petit b - 1er

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <span className='Test'>ceci est un essai</span>
-        </p>
-      </div>
-    );
-  }
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return <Layout />;
 }
 
 export default App;
