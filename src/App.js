15e518e1cd821c79e8e3370ec0965dd0d47ef276
ALEM Guillaume <Guillaume.ALEM@maif.fr>
2021-08-30 14:20:07 +0200

    test const

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <span className='Test'>ceci est un essai</span>
-        </p>
-      </div>
-    );
-  }
+const App = () => (
+     
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      <span className='Test'>ceci est un essai</span>
+    </p>
+  </div>
+)
+  
 
 
 export default App;
